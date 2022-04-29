9cb57bbf066cc9a78b47e1c89f482b529aeb38e9
MikklJ <micheljim101@gmail.com>
2022-04-28 20:07:58 -0700

    Implemented backend (database) + test

---
+++ app/src/main/java/com/example/zooseeker/LocationsListItemDao.java
@@ -0,0 +1,37 @@
+package com.example.zooseeker;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface LocationsListItemDao {
+    @Insert
+    long insert(LocationsListItem locationsListItem);
+
+    @Insert
+    List<Long> insertAll(List<LocationsListItem> locationsListItem);
+
+    @Query("SELECT * from `locations_list_items` WHERE `id`=:id")
+    LocationsListItem get(long id);
+
+    @Query("SELECT * from `locations_list_items` ORDER BY `order`")
+    List<LocationsListItem> getAll();
+
+    @Query("SELECT * from `locations_list_items` ORDER BY `order`")
+    LiveData<List<LocationsListItem>> getAllLive();
+
+    @Update
+    int update(LocationsListItem locationsListItem);
+
+    @Delete
+    int delete(LocationsListItem locationsListItem);
+
+    @Query("SELECT `order` + 1 FROM `locations_list_items` ORDER BY `order` DESC LIMIT 1")
+    int getOrderForAppend();
+}
